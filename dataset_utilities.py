cb24d908962dea1f0047bdf7873bb8da721b86fc
Jayabrata <joyj03787@gmail.com>
2024-03-15 20:08:59 +0530

    update

---
+++ dataset_utilities.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import tarfile
+import time
+import numpy as np
+import pandas as pd
+from packaging import version
+from torch.utils.data import Dataset
+from tqdm import tqdm
+import urllib
+
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = progress_size / (1024.0**2 * duration)
+    percent = count * block_size * 100.0 / total_size
+
+    sys.stdout.write(
+        f"\r{int(percent)}% | {progress_size / (1024.**2):.2f} MB "
+        f"| {speed:.2f} MB/s | {duration:.2f} sec elapsed"
+    )
+    sys.stdout.flush()
+
+def download_dataset():
+    source = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+    target = "aclImdb_v1.tar.gz"
+
+    if os.path.exists(target):
+        os.remove(target)
+    
+    if not os.path.isdir("aclImdb") and not os.path.isfile("aclImdb_v1.tar.gz"):
+        urllib.request.urlretrieve(source, target, reporthook)
+
+    if not os.path.isdir("aclImdb"):
+        with tarfile.open(target, "r:gz") as tar:
+            tar.extractall()
+
